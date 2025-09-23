61f88ca075e76f0b034c5f30a3eac0d39ce37473
Emmett Leahy <C00311509@setu.ie>
2025-09-23 11:50:08 +0000

    update

---
+++ FailedLogin.py
@@ -0,0 +1,14 @@
+from collections import defaultdict
+
+
+counts = defaultdict(int)           # Create a dictionary to keep track of IPs
+
+with open("sample_auth_small.log") as f:
+    for line in f:
+        if "Failed password" in line or "Invalid user" in line:
+            # extract ip
+            ip = ip_parse(line)
+            if ip:
+                counts[ip] += 1
+print(counts)
+
